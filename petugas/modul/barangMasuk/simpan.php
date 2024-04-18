884572c0c5ae6eb97c25f649dd9a0b701eadf814
Muhammad Rico Ardiansyah <mriko743@gmail.com>
2024-04-18 15:43:45 +0700

    update logic

--- petugas/modul/barangMasuk/simpan.php
+++ petugas/modul/barangMasuk/simpan.php
@@ -1,5 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="">
+	<meta name="author" content="">
+	<title>
+		<?php echo $judul; ?>
+	</title>
+	<!-- SweetAlert2 -->
+	<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+</head>
+<body>
 <?php 
-include "sesi_admin.php";
+include "sesi_petugas.php";
 if(isset($_POST['simpan'])) {
 	include('../koneksi.php');
 	$tanggal 	= $_POST['tanggal'];
@@ -17,28 +32,44 @@ if(isset($_POST['simpan'])) {
 	$cek = mysqli_fetch_row($sql_cek);
 
 	if ($cek) {
-		echo "<script>alert('No Invoice sudah ada')</script>";
-		echo '<script>window.history.back()</script>';
-	}else {
-		$tambahStok 	= $stok + $jml_masuk;
+		echo "<script>
+		Swal.fire({
+			icon: 'error',
+			title: 'Oops...',
+			text: 'No Invoice sudah ada!'
+		}).then((result) => {
+			window.history.back();
+		});
+		</script>";
+	} else {
+		$tambahStok = $stok + $jml_masuk;
 
 		$update = ("UPDATE tb_barang SET stok = '". $tambahStok ."' WHERE kode_brg = '". $kode_brg ."' ");
 		$result = mysqli_query($koneksi, $update) or die(mysql_error());
 	
 		$sql = "INSERT INTO tb_barang_in SET tanggal='$tanggal', noinv='$noinv', supplier='$supplier', kode_brg='$kode_brg', nama_brg='$nama_brg', stok='$stok', jml_masuk='$jml_masuk', jam='$jam', keterangan='$keterangan', petugas='$petugas'";
 		mysqli_query($koneksi, $sql);
-		if($sql){
-			 //echo '<script>window.history.back()</script>';
-			header("location: ?m=barangMasuk&s=awal");
-		}else{
-			var_dump($sql);
-			echo "gagal";
+		if(mysqli_affected_rows($koneksi) > 0) {
+			echo "<script>
+			Swal.fire({
+				icon: 'success',
+				title: 'Sukses',
+				text: 'Data berhasil disimpan!'
+			}).then((result) => {
+				window.location.href = '?m=barangMasuk&s=awal';
+			});
+			</script>";
+		} else {
+			echo "<script>
+			Swal.fire({
+				icon: 'error',
+				title: 'Oops...',
+				text: 'Gagal menyimpan data!'
+			});
+			</script>";
 		}
 	}
-
-	
 }
-
-
-
- ?>
+?>
+</body>
+</html>
