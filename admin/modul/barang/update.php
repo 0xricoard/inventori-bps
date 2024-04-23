4d93fe8d931bdba174efe79bdaf84d71c80dd1e2
Muhammad Rico Ardiansyah <mriko743@gmail.com>
2024-04-23 10:10:44 +0700

    fix db table name

--- admin/modul/barang/update.php
+++ admin/modul/barang/update.php
@@ -7,12 +7,12 @@
 	$nama_brg = $_POST['nama_brg'];
 	$satuan = $_POST['satuan'];
 	$stok = $_POST['stok'];
-	$rak = $_POST['rak'];
+	$tim = $_POST['tim'];
 	$supplier = $_POST['supplier'];
 
 	}
 
-	$sql = "UPDATE tb_barang SET nama_brg='$nama_brg', satuan='$satuan', stok='$stok', rak='$rak', supplier='$supplier' WHERE kode_brg='$kode_brg'";
+	$sql = "UPDATE tb_barang SET nama_brg='$nama_brg', satuan='$satuan', stok='$stok', tim='$tim', supplier='$supplier' WHERE kode_brg='$kode_brg'";
 	$update = mysqli_query($koneksi, $sql);
 
 	if ($update) {
