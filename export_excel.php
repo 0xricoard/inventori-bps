d6a973e18379390b87a111515a25d2d4ee580840
Muhammad Rico Ardiansyah <mriko743@gmail.com>
2024-05-30 13:06:59 +0700

    menambah cell untuk stok terakhir

--- export_excel.php
+++ export_excel.php
@@ -15,11 +15,12 @@ $sheet = $spreadsheet->getActiveSheet();
 $sheet->setCellValue('A1', 'Tanggal');
 $sheet->setCellValue('B1', 'Kode Barang');
 $sheet->setCellValue('C1', 'Nama Barang');
-$sheet->setCellValue('D1', 'Stok Terakhir');
+$sheet->setCellValue('D1', 'Stok');
 $sheet->setCellValue('E1', 'Jumlah Masuk');
 $sheet->setCellValue('F1', 'Jumlah Keluar');
-$sheet->setCellValue('G1', 'Keterangan');
-$sheet->setCellValue('H1', 'Nama');
+$sheet->setCellValue('G1', 'Stok Terakhir'); // Tambahkan kolom Stok Terakhir
+$sheet->setCellValue('H1', 'Keterangan');
+$sheet->setCellValue('I1', 'Nama');
 
 // Query untuk mendapatkan data dari database untuk tabel tb_barang_out
 $sql_out = "SELECT tanggal_out AS tanggal, kode_brg, nama_brg, stok, NULL AS jml_masuk, jml_keluar, keterangan, petugas 
@@ -30,20 +31,23 @@ $sql_out = "SELECT tanggal_out AS tanggal, kode_brg, nama_brg, stok, NULL AS jml
             ORDER BY tanggal, nama_brg";
 $result = mysqli_query($koneksi, $sql_out);
 
-
 // Inisialisasi baris awal
 $row = 2;
 
 // Looping untuk mengisi data ke dalam spreadsheet
 while ($row_data = mysqli_fetch_assoc($result)) {
+    // Menghitung stok terakhir
+    $current_stock = $row_data['stok'] + $row_data['jml_masuk'] - $row_data['jml_keluar'];
+    
     $sheet->setCellValue('A'.$row, $row_data['tanggal']);
     $sheet->setCellValue('B'.$row, $row_data['kode_brg']);
     $sheet->setCellValue('C'.$row, $row_data['nama_brg']);
     $sheet->setCellValue('D'.$row, $row_data['stok']);
     $sheet->setCellValue('E'.$row, $row_data['jml_masuk']);
     $sheet->setCellValue('F'.$row, $row_data['jml_keluar']);
-    $sheet->setCellValue('G'.$row, $row_data['keterangan']);
-    $sheet->setCellValue('H'.$row, $row_data['petugas']);
+    $sheet->setCellValue('G'.$row, $current_stock); // Mengisi kolom Stok Terakhir
+    $sheet->setCellValue('H'.$row, $row_data['keterangan']);
+    $sheet->setCellValue('I'.$row, $row_data['petugas']);
     $row++;
 }
 
@@ -55,3 +59,4 @@ header('Cache-Control: max-age=0');
 // Menulis file Excel
 $writer = new Xlsx($spreadsheet);
 $writer->save('php://output');
+?>
